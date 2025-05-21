a293d1e2754f97ded61e1147c9bfe94f3af63ee1
guilhermefragiacomo <guilhermefragiacomo1@gmail.com>
2025-05-21 16:32:39 -0300

    building placement system updated

--- scripts/isometric.gd
+++ scripts/isometric.gd
@@ -6,6 +6,15 @@ extends Node2D
 @onready var preview_building = $Container/SprHouse
 @onready var preview_material = preview_building.material as ShaderMaterial
 
+var building = {
+	"simple_house": {
+		"source_id" = 1,
+		"x_horizontal" = 2,
+		"y_horizontal" = 1,
+		"height" = 3
+		}
+}
+
 func get_all_tile_map_layers(parent_node):
 	var tile_map_layers = []
 	for child in parent_node.get_children():
@@ -13,7 +22,6 @@ func get_all_tile_map_layers(parent_node):
 			tile_map_layers.append(child)
 	return tile_map_layers
 
-var occupied_tiles = {}
 var tile_map_layers = []
 
 var tile_offsets = {
@@ -21,48 +29,66 @@ var tile_offsets = {
 	1: Vector2(0, 0),
 }
 
-func place_building(tile_pos: Vector2i):
+func check_if_can_build(tile_pos: Vector2i, building: Dictionary) -> bool:
+	var tile_map_layer_index = find_tile_map_layer_index(tile_pos)
+	var tile_map_layer = tile_map_layers[tile_map_layer_index]
 	
+	var tile_set_source = building.get("simple_house").get("source_id")
+	
+	var x_horizontal = building.get("simple_house").get("x_horizontal")
+	var y_horizontal = building.get("simple_house").get("y_horizontal")
+	var vertical = building.get("simple_house").get("height")
+	
+	var x_index = tile_pos.x - 1
+	var y_index = tile_pos.y
+	
+	var tile_map_layer_index_temp = tile_map_layer_index
 	var free_space = true
-	if occupied_tiles.has(tile_pos):
-		free_space = false
-	if occupied_tiles.has(Vector2i(tile_pos.x - 1, tile_pos.y)):
-		free_space = false
-	if occupied_tiles.has(Vector2i(tile_pos.x, tile_pos.y - 1)):
-		free_space = false
-	if occupied_tiles.has(Vector2i(tile_pos.x - 1, tile_pos.y - 1)):
-		free_space = false
-		
-	if free_space:
-		occupied_tiles[tile_pos] = true
-		occupied_tiles[Vector2i(tile_pos.x - 1, tile_pos.y)] = true
-		occupied_tiles[Vector2i(tile_pos.x, tile_pos.y - 1)] = true
-		occupied_tiles[Vector2i(tile_pos.x - 1, tile_pos.y - 1)] = true
-		
+	
+	for y_atlas_check in range(vertical - 1, -1, -1):
+		var y_atlas_check_temp = 0
+		for x_atlas_check in range(0, x_horizontal):
+			if (tile_map_layers[tile_map_layer_index_temp + 1].get_cell_atlas_coords(Vector2i(x_index, y_index)).x != -1):
+				free_space = false
+			x_index += 1
+			y_atlas_check_temp = x_atlas_check
+		x_index -= 1
+		for l in range(0, y_horizontal):
+			y_index -= 1
+			y_atlas_check_temp += l + 1
+			if (tile_map_layers[tile_map_layer_index_temp + 1].get_cell_atlas_coords(Vector2i(x_index, y_index)).x != -1):
+				free_space = false
+		tile_map_layer_index_temp += 1
+		x_index -= x_horizontal
+	return free_space
+
+func place_building(tile_pos: Vector2i, building: Dictionary):
+	if (check_if_can_build(tile_pos, building)):
 		var tile_map_layer_index = find_tile_map_layer_index(tile_pos)
 		var tile_map_layer = tile_map_layers[tile_map_layer_index]
 		
-		var tile_set = tile_map_layer.tile_set
-		var tile_set_source = tile_set.get_source(1)
+		var tile_set_source = building.simple_house.source_id
 		
-		var horizontal = 0
-		for i in range(0, tile_set_source.get_tiles_count()-1):
-			if (tile_set_source.has_tile(Vector2i(i, 0))):
-				horizontal += 1
-		var vertical = tile_set_source.get_tiles_count()/horizontal
+		var x_horizontal = building.get("simple_house").get("x_horizontal")
+		var y_horizontal = building.get("simple_house").get("y_horizontal")
+		var vertical = building.get("simple_house").get("height")
+				
+		var x_index = tile_pos.x - 1
+		var y_index = tile_pos.y
 		
-		var atlas_coords = tile_map_layer.get_cell_atlas_coords(tile_pos)
-		
-		var x_index = tile_pos.x
-		var y_index = tile_pos.y + 1
-		print("Camada ", tile_map_layer_index + 2)
-		for i in range(vertical-1, -1, -1):
-			for j in range(0, horizontal):
-				tile_map_layers[tile_map_layer_index + 1].set_cell(Vector2i(x_index, y_index), 1, Vector2i(j, i))
+		for y_atlas_place in range(vertical-1, -1, -1):
+			var y_atlas_place_temp = 0
+			for x_atlas_place in range(0, x_horizontal):
+				tile_map_layers[tile_map_layer_index + 1].set_cell(Vector2i(x_index, y_index), 1, Vector2i(x_atlas_place, y_atlas_place))
 				x_index += 1
+				y_atlas_place_temp = x_atlas_place
+			x_index -= 1
+			for k in range(0, y_horizontal):
 				y_index -= 1
+				y_atlas_place_temp += k + 1
+				tile_map_layers[tile_map_layer_index + 1].set_cell(Vector2i(x_index, y_index), 1, Vector2i(y_atlas_place_temp, y_atlas_place))
 			tile_map_layer_index += 1
-			x_index -= 4
+			x_index -= x_horizontal
 
 func resize_zoom(value: int):
 	if (zoom + value >= 1):
@@ -77,7 +103,7 @@ func _ready() -> void:
 func _unhandled_input(event):
 	if event.is_action_pressed("place_building"):
 		var tile_pos = get_mouse_tile_coords()
-		place_building(tile_pos)
+		place_building(tile_pos, building)
 
 func set_preview_valid(is_valid: bool):
 	if is_valid:
@@ -92,24 +118,12 @@ func _process(delta: float):
 		resize_zoom(-1)
 	
 	var tile_pos = get_mouse_tile_coords()
-	var free_space = true
-	if occupied_tiles.has(tile_pos):
-		free_space = false
-	if occupied_tiles.has(Vector2i(tile_pos.x - 1, tile_pos.y)):
-		free_space = false
-	if occupied_tiles.has(Vector2i(tile_pos.x, tile_pos.y - 1)):
-		free_space = false
-	if occupied_tiles.has(Vector2i(tile_pos.x - 1, tile_pos.y - 1)):
-		free_space = false
 	
-	set_preview_valid(free_space)
+	set_preview_valid(check_if_can_build(tile_pos, building))
 	
 	var tile_map_layer = tile_map_layers[find_tile_map_layer_index(tile_pos)]
 	
 	var atlas_coords = tile_map_layer.get_cell_atlas_coords(tile_pos)
-	if (atlas_coords.x == -1):
-		atlas_coords.x = 0
-		set_preview_valid(false)
 	var offset = tile_offsets.get(atlas_coords.x, Vector2.ZERO)
 	
 	var world_pos = tile_map_layer.map_to_local(tile_pos)
